4dd5f34652f158984169debb82ffbd4d21e16cce
Anton Skripko <ant.skripko@gmail.com>
2019-11-24 16:54:27 +0200

    initial commit

---
+++ Homework5/MapaMagicViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Homework5
+//
+//  Created by Developer on 23.11.2019.
+//  Copyright © 2019 Developer. All rights reserved.
+//
+
+import UIKit
+
+class MapaMagicViewController: UIViewController {
+
+    @IBOutlet weak var btnOpen: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btnOpen.layer.cornerRadius = 15
+    }
+
+
+}
+
